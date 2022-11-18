311eca3b1923fd62de591d52ed8301ef24b697cb
Kumard <kumard@rbos.co.uk>
2022-11-18 17:45:09 +0530

    readnow page protection added

--- src/Components/ReadNow/ReadNow.jsx
+++ src/Components/ReadNow/ReadNow.jsx
@@ -1,8 +1,10 @@
 import React,{useEffect, useState} from 'react'
 import './ReadNow.css'
+import { useNavigate } from "react-router-dom";
 // import { NavItem } from 'react-bootstrap'
 
 export default function ReadNow() {
+  const navigate = useNavigate();  //variable to protect the page
   const [news, setNews] = useState([])
 
   useEffect(() => {    
@@ -16,6 +18,27 @@ export default function ReadNow() {
     
   }, [])
   
+
+  useEffect(() => {                                         //coding start to protect the page
+    fetch("http://localhost:9000/auth/isAuthenticated", {
+        method: "POST",
+        headers: {
+            "Authorization": `Bearer ${localStorage.getItem("jwt_token")}`
+        }
+    })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            // if(data.status===401){
+            //     navigate("/login")
+            // }
+
+            if (!data.isAuthenticated) {
+                navigate("/login")
+            }
+        })
+}, [])
+
 
   return (
     <div className="container user-Readnow mx-auto">
